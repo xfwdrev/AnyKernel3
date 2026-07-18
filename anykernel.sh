4b9567dd79884289b3fcbbd57f6c0d82310b799c
xfwdrev <74693315+xfwdrev@users.noreply.github.com>
2026-07-19 01:41:12 +0800

    anykernel.sh: S21 -> Z Flip4
    Signed-off-by: xfwdrev <74693315+xfwdrev@users.noreply.github.com>

--- anykernel.sh
+++ anykernel.sh
@@ -1,36 +1,38 @@
 ### AnyKernel3 Ramdisk Mod Script
 ## osm0sis @ xda-developers
-# chiclet kernel adapted from: blu_spark pxl7a by eng.stk
 
 ### AnyKernel setup
 # global properties
 properties() { '
-kernel.string=ChicletKernel for Galaxy S21 Series
+kernel.string=ChicletKernel for Galaxy Z Flip4
 do.devicecheck=1
 do.modules=0
-do.systemless=1
+do.systemless=0
 do.cleanup=1
 do.cleanuponabort=0
-device.name1=t2s
-supported.versions=16
+device.name1=b4q
+supported.versions=
 supported.patchlevels=
 '; } # end properties
 
 
 ### AnyKernel install
 # boot shell variables
-BLOCK=/dev/block/by-name/boot;
-IS_SLOT_DEVICE=0;
+BLOCK=boot;
+IS_SLOT_DEVICE=auto;
 RAMDISK_COMPRESSION=auto;
 PATCH_VBMETA_FLAG=auto;
 NO_MAGISK_CHECK=1;
-NO_VBMETA_PARTITION_PATCH=1;
 
 # import functions/variables and setup patching - see for reference (DO NOT REMOVE)
 . tools/ak3-core.sh;
 
 # boot install
-flash_generic boot;
-flash_generic vendor_boot;
-flash_generic dtbo;
+split_boot
+if [ -f "split_img/ramdisk.cpio" ]; then
+    unpack_ramdisk
+    write_boot
+else
+    flash_boot
+fi
 ## end boot install
